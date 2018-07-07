ee958d816540bc44c91e9d92fc253e7204794ea4
Jan Hořava <czechinthemail@gmail.com>
2018-07-07 13:26:01 -0700

    switched to Number.parseInt()

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
 
 function resize(){
     document.getElementById(document.getElementsByClassName('mce-edit-area')[0].firstChild.id).style.height =
-      parseInt(
+      Number.parseInt(
         window.innerHeight - 55,
         10
       ) + 'px';
